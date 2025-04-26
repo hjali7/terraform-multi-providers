b5da41e76fae8d1045a551123c25edb7b07d8f25
hjali7 <hjikingali@gmail.com>
2025-04-26 21:56:46 +0330

    edit localstack/main.tf

---
+++ localstack-project/main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_version = ">= 1.0.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    access_key                  = "test"
+    secret_key                  = "test"
+    region                      = var.region
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    s3_force_path_style         = true
+
+    endpoints {
+        s3 = "http://localhost:4566"
+    }
+}
+
+resource "aws_s3_bucket" "first_bucket" {
+    bucket = var.bucket_name
+}
