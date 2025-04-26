60e72e914ad1b458800db9b037dd7198cf585102
hjali7 <hjikingali@gmail.com>
2025-04-26 23:06:25 +0330

    bug fixed

--- localstack-project/main.tf
+++ localstack-project/main.tf
@@ -1,25 +1,28 @@
 terraform {
-    required_version = ">= 1.0.0"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
 }
 
 provider "aws" {
-    access_key                  = "test"
-    secret_key                  = "test"
-    region                      = var.region
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
+  access_key                  = "test"
+  secret_key                  = "test"
+  token                       = "dummy"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style            = true
 
-    endpoints {
-        s3 = "http://localhost:4566"
-    }
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
 }
 
 resource "aws_s3_bucket" "first_bucket" {
-    bucket = var.bucket_name
+  bucket = var.bucket_name
+  force_destroy = true
 }
--- localstack-project/outputs.tf
+++ localstack-project/outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
-  description = "The name of the S3 bucket created by LocalStack."
-  value = aws_s3_bucket.first_bucket.bucket
+  description = "The name of the created S3 bucket"
+  value       = aws_s3_bucket.first_bucket.bucket
 }
